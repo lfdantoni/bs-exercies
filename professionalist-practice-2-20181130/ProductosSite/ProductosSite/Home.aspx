c4d7fa4d1af1f4e2b6137b1db9351692df4032d9
lfdantoni <lfdantoni@gmail.com>
2018-12-01 16:46:16 -0300

    ProfPrac2: Init project and added home page with buttons

---
+++ professionalist-practice-2-20181130/ProductosSite/ProductosSite/Home.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Products.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="ProductosSite.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="GVProducts" runat="server">
+        <RowStyle BackColor="White" ForeColor="#003399" /> 
+    </asp:GridView>
+</asp:Content>
---
+++ professionalist-practice-2-20181130/ProductosSite/ProductosSite/Home.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProductosSite
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connectionString = WebConfigurationManager.ConnectionStrings["ConnStringProducts"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+
+                    sqlConnection.Open();
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT *  FROM [BaseProductos].[dbo].[Productos]", sqlConnection);
+
+                    DataSet dataSet = new DataSet();
+
+                    sqlDataAdapter.Fill(dataSet);
+
+                    GVProducts.DataSource = dataSet.Tables[0];
+                    GVProducts.DataBind();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                //Write code here to handle exception
+
+            }
+        }
+    }
+}
